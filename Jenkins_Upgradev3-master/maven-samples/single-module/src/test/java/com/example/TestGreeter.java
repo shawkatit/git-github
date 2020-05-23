302be00d6b52d73ff0e18b2443a72e5e487b6b4b
Shawkat, Ali <dmshawkat@yahoo.com>
2020-05-23 18:56:32 +0000

    5th try

--- Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks shawkat making me Happy";
+    String someone = "World, Thanks you  making us Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
